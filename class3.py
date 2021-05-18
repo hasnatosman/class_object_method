50b17b159e60761f827f68ba4b1d7da0a9b41f06
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-05-18 20:07:41 +0600

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,19 @@
+class Car:
+    name = ''
+    color = ''
+
+    def start():
+        print('Starting the car!!!!!!!!!!!!!!!!!')
+
+
+Car.name = 'Axio'
+Car.color = 'Yellow'
+
+
+print('Name of the car: ', Car.name)
+print('Color of the car: ', Car.color)
+print()
+print()
+
+
+Car.start()
